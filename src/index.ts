8f01074a05f5dc61fe1665ffef478c8ecbef942c
sidtric <siddharthfuloria06@gmail.com>
2026-05-12 11:16:53 +0530

    fix: allow localhost:3002 in CORS origins

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ app.use(helmet({ contentSecurityPolicy: false, crossOriginResourcePolicy: { poli
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
+  'http://localhost:3002',
   'http://localhost:5173',
   'https://yourorchard.in',
   'https://www.yourorchard.in',
